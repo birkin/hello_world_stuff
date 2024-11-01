75c4c7f6d63e729b1b5f22d65b3fe5491450318a
Birkin James Diana <birkin.diana@gmail.com>
2024-11-01 14:45:31 -0400

    adds python version printout.

--- 03_imports.py
+++ 03_imports.py
@@ -1,5 +1,9 @@
+import sys
+
 import django
 import httpx
 
+major, minor, micro = sys.version_info[:3]
+print(f'using python version, ``{major}.{minor}.{micro}``')
 print(f'using django version, ``{django.get_version()}``')
 print(f'using httpx version, ``{httpx.__version__}``')
